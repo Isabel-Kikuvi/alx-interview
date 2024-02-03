d300c7fafa5b8f2f36d9eeb6b01eb9d4c7f00678
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2024-02-03 01:01:18 -0800

    minor edits

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,8 +14,12 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
